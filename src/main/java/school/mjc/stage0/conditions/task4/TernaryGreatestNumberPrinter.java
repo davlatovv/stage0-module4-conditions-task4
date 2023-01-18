49a3083ab9aefdb2b7ca105f6d2ff41daaaa148e
davlatovv <zafargithub@gmail.com>
2023-01-18 16:39:21 +0500

    Ternary operstors

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
-        int great = first>second?first:second;
+        int great = first > second?first:second;
         System.out.println(great);
     }
 }
